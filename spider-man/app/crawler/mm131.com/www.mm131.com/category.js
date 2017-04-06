8f4a4b1446a9158b4e00641efb892ed9f50224d6
锤子 <chuigengchen@gmail.com>
2017-04-06 23:19:52 +0800

    尝试爬取页面

--- spider-man/app/crawler/mm131.com/www.mm131.com/category.js
+++ spider-man/app/crawler/mm131.com/www.mm131.com/category.js
@@ -1,6 +1,45 @@
 'use strcit';
 
+const _ = require('underscore');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+const request = require('co-request');
+const URL = require('url-parse');
+
 // 分类页
-exports.work = function* () {
+exports.work = function* (url) {
+  // 下载页面
+  let response = yield request({
+    uri: url,
+    encoding: null,
+    gzip: true,
+  });
+  let html = iconv.decode(response.body, 'GB2312');
+
+  // 解析页面元素  
+  let $ = cheerio.load(html);
   
+  let itemUrls = [];
+  $('div[class=main] dl[class="list-left public-box"] dd').each(function(i, elem) {
+    let itemUrl = $(this).children('a').attr('href');
+    let url = new URL(itemUrl);
+    if (/^\/\w+\/\d+\.html$/.test(url.pathname)) {
+      itemUrls.push(itemUrl);
+    }
+  });
+
+  let nextPageUrl;
+  $('div[class=main] dd[class=page] a').each(function(i, elem) {
+    let pageUrl = $(this).attr('href');
+    let pageText = $(this).text();
+    if (pageText === '下一页') {
+      nextPageUrl = pageUrl;
+    }
+  });
+
+  // TODO 统一把相对路径转换为绝对路径
+  return {
+    resourceUrls: _.extend(itemUrls, [nextPageUrl]),
+    troubleMakers: []
+  };
 };
--- spider-man/app/crawler/mm131.com/www.mm131.com/index.js
+++ spider-man/app/crawler/mm131.com/www.mm131.com/index.js
@@ -1,7 +1,9 @@
 'use strcit';
 
+const cheerio = require('cheerio');
 const iconv = require('iconv-lite');
-const request = require('co-request')
+const request = require('co-request');
+const URL = require('url-parse');
 
 // 首页
 exports.work = function* (url) {
@@ -13,9 +15,21 @@ exports.work = function* (url) {
   });
   let html = iconv.decode(response.body, 'GB2312');
 
-  // 解析页面元素
-  
+  // 解析页面元素  
+  let $ = cheerio.load(html);
+
   // 提取需要的信息
-  
-  // 返回
+  let categoryUrls = [];
+  $('div[class=nav] ul li').each(function(i, elem) {
+    let categoryUrl = $(this).children('a').attr('href');
+    let url = new URL(categoryUrl);
+    if (/^\/(\w)+\/$/.test(url.pathname)) {
+      categoryUrls.push(categoryUrl);
+    }
+  });
+
+  return {
+    resourceUrls: categoryUrls,
+    troubleMakers: []
+  };
 };
